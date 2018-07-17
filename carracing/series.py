53c7dae0f9ce8993955d228454bf2f382d0c66f5
Andrew Salamon <bleyddyn.aprhys@gmail.com>
2018-07-17 12:31:23 -0700

    Rewrote series.py to work on one file at a time. Inefficient, but it looks like it works.

--- carracing/series.py
+++ carracing/series.py
@@ -27,14 +27,14 @@ def load_raw_data_list(filelist):
   for i in range(len(filelist)):
     filename = filelist[i]
     raw_data = np.load(os.path.join(DATA_DIR, filename))
-    #data_list.append(raw_data['obs'])
+    data_list.append(raw_data['obs'])
     action_list.append(raw_data['action'])
     #print( "File {}: {} {}".format( filename, len(raw_data['action']), len(raw_data['obs']) ) )
     #if ((i+1) % 1000 == 0):
     #  print("loading file", (i+1))
-  return data_list, action_list
+  return np.squeeze(np.array(data_list)), np.squeeze(np.array(action_list))
 
-def encode_batch(batch_img):
+def encode_batch(vae, batch_img):
   simple_obs = np.copy(batch_img).astype(np.float)/255.0
   simple_obs = simple_obs.reshape(batch_size, 64, 64, 3)
   mu, logvar = vae.encode_mu_logvar(simple_obs)
@@ -59,37 +59,32 @@ filelist = os.listdir(DATA_DIR)
 filelist.sort()
 #filelist = filelist[0:10000]
 
-dataset, action_dataset = load_raw_data_list(filelist)
-
-gen = VaeDataGenerator( filelist, batch_size=batch_size, shuffle=False, max_load=10000 )
-num_batches = len(gen)
-
-reset_graph()
-
-vae = ConvVAE(z_size=z_size,
-              batch_size=batch_size,
-              learning_rate=learning_rate,
-              kl_tolerance=kl_tolerance,
-              is_training=False,
-              reuse=False,
-              gpu_mode=True) # use GPU on batchsize of 1000 -> much faster
-
-vae.load_json(os.path.join(model_path_name, 'vae.json'))
-
 # Need to rewrite this so it loads one file at a time, recreating ConvVAE with a batch_size from the loaded data
+action_dataset = []
 mu_dataset = []
 logvar_dataset = []
-for i in range(num_batches):
-  data_batch = gen[i]
-  if data_batch.shape[0] == 0:
-    continue
+for afile in filelist:
+    dataset, actions = load_raw_data_list([afile])
+    batch_size = len(dataset)
+    print( "{}: {} {}".format( afile, dataset.shape, actions.shape ) )
 
-  #data_batch = dataset[i]
-  mu, logvar, z = encode_batch(data_batch)
-  mu_dataset.append(mu.astype(np.float16))
-  logvar_dataset.append(logvar.astype(np.float16))
-  if ((i+1) % 100 == 0):
-    print(i+1)
+    reset_graph()
+
+    vae = ConvVAE(z_size=z_size,
+                  batch_size=batch_size,
+                  learning_rate=learning_rate,
+                  kl_tolerance=kl_tolerance,
+                  is_training=False,
+                  reuse=False,
+                  gpu_mode=True) # use GPU on batchsize of 1000 -> much faster
+
+    vae.load_json(os.path.join(model_path_name, 'vae.json'))
+
+    data_batch = dataset
+    mu, logvar, z = encode_batch(vae, data_batch)
+    action_dataset.append(actions)
+    mu_dataset.append(mu.astype(np.float16))
+    logvar_dataset.append(logvar.astype(np.float16))
 
 action_dataset = np.array(action_dataset)
 mu_dataset = np.array(mu_dataset)
